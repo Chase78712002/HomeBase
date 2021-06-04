86a1b90c7faa2205f737fd4c0d41dcf11fbcf6c2
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-06-04 10:24:29 -0700

    Change namespace: api to test (and in other locations as a test to see how react works with rails)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :api do # /api/data
+  namespace :test do # /api/data
 
     get '/data', to: 'tests#index'
     
