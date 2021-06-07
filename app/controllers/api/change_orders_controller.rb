94b1c4a25c335f8b61fe4e4534c502142ac43bf9
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-06-07 10:43:19 -0700

    Setup milestone controller

--- app/controllers/api/change_orders_controller.rb
+++ app/controllers/api/change_orders_controller.rb
@@ -6,33 +6,33 @@ class ChangeOrdersController < ApplicationController
   end
 
   def create
-    @change_orders = ChangeOrder.new change_orders_params
-    if @change_orders.save
-      render json: @change_orders
+    @change_order = ChangeOrder.new change_order_params
+    if @change_order.save
+      render json: @change_order
     else
-      render json: { error: change_orders.errors.messages }, status: 422
+      render json: { error: change_order.errors.messages }, status: 422
     end
   end
 
   def update
-    @change_orders = ChangeOrder.find params[:id]
-    @change_orders.update_attributes change_orders_params # setup which attributes we want to be read only etc
-    render json: @change_orders
+    @change_order = ChangeOrder.find params[:id]
+    @change_order.update_attributes change_order_params # setup which attributes we want to be read only etc
+    render json: @change_order
   end
 
   def destroy
-    @change_orders = ChangeOrder.find params[:id]
+    @change_order = ChangeOrder.find params[:id]
 
-    if @change_orders.destroy
+    if @change_order.destroy
       head :no_content
     else
-      render json: { error: change_orders.errors.messages }, status: 422
+      render json: { error: change_order.errors.messages }, status: 422
     end
   end
 
   private
     def change_orders_params
-      params.require(:change_order_params).permit(
+      params.require(:change_order).permit(
         :description,
         :cost,
         :approval,
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'index/create'
+  get 'index/update'
+  get 'index/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root to: 'landingpage#index'
