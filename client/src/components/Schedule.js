a59e3b016b479a18557f502a9afe66a996a7a9e6
Lily Habberfield <lilyhabbs@gmail.com>
2021-06-07 18:50:00 +0000

    Create template for section components

---
+++ client/src/components/Schedule.js
@@ -0,0 +1,10 @@
+import "./App.scss";
+
+export default function Schedule() {
+  return (
+    <section className="content">
+      <h1>Schedule</h1>
+      <p>This is where the project milestones timeline will go.</p>
+    </section>
+  )
+}
