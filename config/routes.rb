b65febf50cc65c0a3f944b8f9941bc6f095233c3
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-06-07 12:31:11 -0700

    Change to routes, landingpage to projects for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root to: 'landingpage#index'
+  root to: 'projects#index'
 
   namespace :api do # /api/data
 
