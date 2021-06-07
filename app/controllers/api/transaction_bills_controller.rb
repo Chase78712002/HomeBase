e9b27b16c34679d5f997ebbd39d382f4f6a6f512
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-06-07 10:49:52 -0700

    Setup transaction_bill controller

---
+++ app/controllers/api/transaction_bills_controller.rb
@@ -0,0 +1,50 @@
+class TransactionBillsController < ApplicationController
+  def index
+    @transaction_bills = TransactionBill.all
+
+    render json: @transaction_bills
+  end
+
+  def create
+    @transaction_bill = TransactionBill.new transaction_bill_params
+    if @transaction_bill.save
+      render json: @transaction_bill
+    else
+      render json: { error: transaction_bill.errors.messages }, status: 422
+    end
+  end
+
+  def show
+    @transaction_bill = TransactionBill.find params[:id]
+
+    render json: @transaction_bill
+  end
+
+  def update
+    @transaction_bill = TransactionBill.find params[:id]
+    @transaction_bill.update_attributes transaction_bill_params # setup which attributes we want to be read only etc
+    render json: @transaction_bill
+  end
+
+  def destroy
+    @transaction_bill = TransactionBill.find params[:id]
+
+    if @transaction_bill.destroy
+      head :no_content
+    else
+      render json: { error: transaction_bill.errors.messages }, status: 422
+    end
+  end
+
+  private
+    def transaction_bill_params
+      params.require(:transaction_bill).permite(
+        :description,
+        :amount,
+        :date,
+        :change_order_id,
+        :milestone_id,
+        :budget_category_id
+      )
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'index/create'
-  get 'index/update'
-  get 'index/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root to: 'landingpage#index'
