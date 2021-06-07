b40657c94e41ebe7eeaf0295efd8e0cd26bf248f
Alvin Lin <alvinch.lin@gmail.com>
2021-06-06 18:53:14 -0700

    Side Nav bar implement.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,15 +6,11 @@ export default function App() {
 
   return (
     <main className="layout">
-      <section className="sidebar">
-        <div className="sidebar__logo sidebar--centered">
-          Insert logo
-        </div>
-        <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu sidebar--centered">
-          <Nav />
-        </nav>
-      </section>
+      
+      <nav className="sidebar__menu sidebar--centered">
+        <Nav />
+      </nav>
+      
       <section className="content">
         <h1>Title of Component</h1>
         <p>This is where the content will go.</p>
