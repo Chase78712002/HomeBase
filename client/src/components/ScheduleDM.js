e5b9ff22d3c51b4dfe6c27146d43b4bd034e5e52
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-06-08 13:11:06 -0700

    Grabbing data from DB to display in schedule

--- client/src/components/ScheduleDM.js
+++ client/src/components/ScheduleDM.js
@@ -1,34 +1,41 @@
 import { React, useEffect, useState } from "react";
 
 import TimelineDm from "./TimelineDM";
+import TimelineItemDM from "./TimelineItemDM";
 
 import "./App.scss";
 import "./Schedule.scss";
 import axios from "axios";
 
 export default function CustomizedTimeline() {
-  // const [schedule, setSchedule] = useState([]);
+  const [schedule, setSchedule] = useState([]);
 
-  // useEffect(() => {
-  //   axios
-  //     .get(`/api/milestones`)
-  //     .then((response) => {
-  //       // console.log(response.data[0].description);
-  //       setSchedule((prev) => ({
-  //         ...prev,
-  //         schedule: response.data[0],
-  //       }));
-  //       console.log("schedule sate:", schedule);
-  //     })
-  //     .catch((error) => {
-  //       const errorMessage = error;
-  //       return errorMessage;
-  //     });
-  // }, []);
+  useEffect(() => {
+    axios
+      .get(`/api/milestones`)
+      .then((response) => {
+        console.log("The response:", response.data);
+        setSchedule(response.data);
+
+        console.log("schedule sate:", schedule);
+      })
+      .catch((error) => {
+        const errorMessage = error;
+        return errorMessage;
+      });
+  }, []);
+
+  const timeline = schedule.map((data, idx) => (
+    <TimelineItemDM
+      data={data}
+      key={idx}
+      statusColor={{ statusColor: "#eb6060" }}
+    />
+  ));
 
   return (
     <>
-      <TimelineDm />
+      <div className="timeline-container">{timeline}</div>
     </>
   );
 }
--- client/src/components/TimelineItemDM.js
+++ client/src/components/TimelineItemDM.js
@@ -6,12 +6,10 @@ const TimelineItemDM = ({ data }) => (
       <span className="tag" style={{ background: data.color }}>
         {data.description}
       </span>
-      <p>Start date: {data.startDate}</p>
-      <p>End date: {data.endDate}</p>
-      <p>{data.status}</p>
-      <p>
-        This is extra content to fill the space here in the box to be a better
-        representation of what it may look like if bigger
+      <p>Start date: {data.start_date}</p>
+      <p>End date: {data.end_date}</p>
+      <p className="status" style={{ background: data.statusColor }}>
+        {data.status}
       </p>
       <span className="circle"></span>
     </div>
