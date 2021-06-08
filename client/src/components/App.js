89fb4500afb36fdc632846e182ce4d243b969dae
Lily Habberfield <lilyhabbs@gmail.com>
2021-06-08 15:32:20 -0600

    Accept merge conflicts

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -34,55 +34,35 @@ const useStyles = makeStyles((theme) => ({
 const menuItems = [
   {
     id: 1,
-<<<<<<< HEAD
-    text: 'Dashboard',
-=======
     text: "Dashboard",
->>>>>>> 0254b128605aa0842903374354212eff8653107c
     icon: <DashboardTwoToneIcon />,
     path: "/dashboard",
     component: Dashboard,
   },
   {
     id: 2,
-<<<<<<< HEAD
-    text: 'Schedule',
-=======
     text: "Schedule",
->>>>>>> 0254b128605aa0842903374354212eff8653107c
     icon: <ScheduleTwoToneIcon />,
     path: "/schedule",
     component: Schedule,
   },
   {
     id: 3,
-<<<<<<< HEAD
-    text: 'Budget',
-=======
     text: "Budget",
->>>>>>> 0254b128605aa0842903374354212eff8653107c
     icon: <LocalAtmTwoToneIcon />,
     path: "/budget",
     component: Budget,
   },
   {
     id: 4,
-<<<<<<< HEAD
-    text: 'Change orders',
-=======
     text: "Change orders",
->>>>>>> 0254b128605aa0842903374354212eff8653107c
     icon: <LoopTwoToneIcon />,
     path: "/change_orders",
     component: ChangeOrders,
   },
   {
     id: 5,
-<<<<<<< HEAD
-    text: 'Documents',
-=======
     text: "Documents",
->>>>>>> 0254b128605aa0842903374354212eff8653107c
     icon: <DescriptionTwoToneIcon />,
     path: "/documents",
     component: Documents,
@@ -104,11 +84,7 @@ export default function App() {
         >
           <List>
             {menuItems.map((item) => (
-<<<<<<< HEAD
-              <Link to={item.path} className={classes.link} key={item.id}>
-=======
               <Link key={item.id} to={item.path} className={classes.link}>
->>>>>>> 0254b128605aa0842903374354212eff8653107c
                 <ListItem button>
                   <ListItemIcon>{item.icon}</ListItemIcon>
                   <ListItemText primary={item.text} />
@@ -120,11 +96,7 @@ export default function App() {
 
         <Switch>
           {menuItems.map((item) => (
-<<<<<<< HEAD
-            <Route path={item.path} component={item.component} key={item.id}/>
-=======
             <Route key={item.id} path={item.path} component={item.component} />
->>>>>>> 0254b128605aa0842903374354212eff8653107c
           ))}
         </Switch>
       </div>
