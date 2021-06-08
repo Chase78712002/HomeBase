f94e983a2d5184460e04ac92bae88e2498f46f7f
Alvin Lin <alvinch.lin@gmail.com>
2021-06-07 23:11:31 -0700

    Align search bar and upload button.

--- client/src/components/Documents.js
+++ client/src/components/Documents.js
@@ -1,14 +1,37 @@
-import { Divider } from "@material-ui/core";
+import { Container, makeStyles } from "@material-ui/core";
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import "./App.scss";
 import DocumentListItem from "./Document/DocumentListItem";
+import SearchBar from "./Document/Searchbar";
 import UploadButtons from "./Document/Upload";
 
+const theme = createMuiTheme({
+  overrides: {
+    MuiContainer: {
+      root: {
+        display: 'flex',
+        'justify-content': 'space-between'
+      },
+    },
+  },
+});
+
+
+
 export default function Documents() {
+  // const classes = useStyles
+
   return (
     <section className="content">
       <h1>Documents</h1>
-      <UploadButtons />
-      <Divider />
+
+      <ThemeProvider theme={theme}>
+        <Container maxWidth="lg" disableGutters='true'>
+          <SearchBar />
+          <UploadButtons />
+        </Container>
+      </ThemeProvider>
+
       <DocumentListItem number={1} />
       <DocumentListItem number={2}/>
       <DocumentListItem number={3} />
