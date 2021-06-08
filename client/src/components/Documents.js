67e4c27ed609c8a36fd098d4396c56ccd70ee979
Alvin Lin <alvinch.lin@gmail.com>
2021-06-07 21:04:31 -0700

    Add a list of file items in Documents.js for testing.

--- client/src/components/Documents.js
+++ client/src/components/Documents.js
@@ -1,12 +1,17 @@
+import { Divider } from "@material-ui/core";
 import "./App.scss";
+import DocumentListItem from "./Document/DocumentListItem";
 import UploadButtons from "./Document/Upload";
 
 export default function Documents() {
   return (
     <section className="content">
       <h1>Documents</h1>
-      <p>This is where the table of documents will go.</p>
       <UploadButtons />
+      <Divider />
+      <DocumentListItem number={1} />
+      <DocumentListItem number={2}/>
+      <DocumentListItem number={3} />
     </section>
   )
 }
