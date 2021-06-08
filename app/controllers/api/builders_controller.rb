c64726b67eb37e66b668b5f5a3fc65cc28515475
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-06-08 16:56:56 -0700

    remove create method from controller

--- app/controllers/api/builders_controller.rb
+++ app/controllers/api/builders_controller.rb
@@ -3,19 +3,5 @@ class Api::BuildersController < ApplicationController
   end
 
   def create
-    builder = Builder.create!(
-      email: params['user']['password']
-      password: params['user']['password']
-      password_confirmation: ['user']['password_confirmation']
-    )
-    if builder
-      session[:builder_id] = builder.id
-      render json: {
-        status: :created,
-        builder: builder
-      }
-    else
-      render json: {status: 500}
-    end
   end
 end
