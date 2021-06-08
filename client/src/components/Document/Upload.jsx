3b2e8c65a1af6cdf98a525041240ab93abdfb7ec
Alvin Lin <alvinch.lin@gmail.com>
2021-06-07 18:07:50 -0700

    Implement Upload button. able to see file details in browser console. Need to implement the feature that will save the file name in our database.

---
+++ client/src/components/Document/Upload.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  input: {
+    display: 'none',
+  },
+}));
+
+export default function UploadButtons() {
+  const classes = useStyles();
+
+
+  const file_details = (event) => {
+    // this console.log the file details.
+    console.log(event.target.files);
+
+  }
+
+  return (
+    <div className={classes.root}>
+      <input
+        accept="image/*, video/*, .pdf"
+        className={classes.input}
+        id="contained-button-file"
+        multiple
+        type="file"
+        onChange={e => file_details(e)}
+      />
+      <label htmlFor="contained-button-file">
+        <Button 
+        variant="contained" 
+        color="primary" 
+        component="span" 
+        size="large"
+        startIcon={<CloudUploadIcon />}
+        >
+          Upload
+        </Button>
+      </label>
+      
+    </div>
+  );
+}
--- client/src/components/Documents.js
+++ client/src/components/Documents.js
@@ -1,10 +1,12 @@
 import "./App.scss";
+import UploadButtons from "./Document/Upload";
 
 export default function Documents() {
   return (
     <section className="content">
       <h1>Documents</h1>
       <p>This is where the table of documents will go.</p>
+      <UploadButtons />
     </section>
   )
 }
