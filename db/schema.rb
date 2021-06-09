aeae0c3c1fecd77745cba58d634ab9d8818fa500
Alvin Lin <alvinch.lin@gmail.com>
2021-06-09 15:36:52 -0700

    merge from master.

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2021_06_09_220006) do
-=======
 ActiveRecord::Schema.define(version: 2021_10_09_053232) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
