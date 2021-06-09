4e93c469021c017d3d47c48c849dc534009f0cd9
Alvin Lin <alvinch.lin@gmail.com>
2021-06-08 23:17:50 -0700

    Remove dummy data.

--- client/src/components/Documents.js
+++ client/src/components/Documents.js
@@ -6,29 +6,6 @@ import AddNewDocument from "./Document/AddNewDocument";
 import DocumentList from "./Document/DocumentList";
 import SearchBar from "./Document/Searchbar";
 import axios from 'axios';
-const docData = [
-  {
-    id: 1,
-    title: "Random small Contract",
-    category_type: "Contract444",
-    path: "user/dmar/documents",
-    project_id: 1,
-  },
-  {
-    id: 2,
-    title: "whatever Floor Plan Option #2",
-    category_type: "Floor Plan222",
-    path: "user/dmar/documents/floorplans",
-    project_id: 1,
-  },
-  {
-    id: 3,
-    title: 'BIG small Window Options',
-    category_type: 'Supplementary123',
-    path: 'user/dmar/documents/windows',
-    project_id: 2
-  }
-];
 
 
 const theme = createMuiTheme({
