5154ce1c8a4c4e08426ff2c10f05e692488faca2
Alvin Lin <alvinch.lin@gmail.com>
2021-06-08 18:43:24 -0700

    Remove unused imports.

--- client/src/components/Document/EditFile.jsx
+++ client/src/components/Document/EditFile.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
 import NativeSelect from '@material-ui/core/NativeSelect';
 import InputBase from '@material-ui/core/InputBase';
-import useContext from 'react';
 
 
 
