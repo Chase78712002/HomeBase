82a9390fd7601179cfeb4a7a0b399d2e0118b4bf
Alvin Lin <alvinch.lin@gmail.com>
2021-06-08 23:14:19 -0700

    Able to load files from database. Able to add new document to database.

--- client/src/components/Document/AddNewDocument.jsx
+++ client/src/components/Document/AddNewDocument.jsx
@@ -13,12 +13,14 @@ import Select from "@material-ui/core/Select";
 import { IconButton } from "@material-ui/core";
 import PostAddRoundedIcon from "@material-ui/icons/PostAddRounded";
 import UploadButtons from "./Upload";
+import axios from "axios";
+import {useHistory} from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   container: {
     display: "flex",
     flexDirection: "column",
-    justifyItems: "space-between"
+    justifyItems: "space-between",
   },
   formControl: {
     margin: theme.spacing(1),
@@ -29,8 +31,12 @@ const useStyles = makeStyles((theme) => ({
 export default function AddNewDocument(props) {
   const classes = useStyles();
   const [open, setOpen] = useState(false);
+  const history = useHistory();
+
+  const [fileName, setFileName] = useState();
   const [category, setCategory] = useState("");
   const [path, setPath] = useState();
+
   const handleChange = (event) => {
     setCategory(event.target.value);
   };
@@ -45,14 +51,36 @@ export default function AddNewDocument(props) {
 
   const handlePath = (path) => {
     setPath(path);
-  }
+  };
 
   useEffect(() => {
     if (path) {
-
       console.log(`this is in AddNewDocument: ${path}`);
     }
-  },[path])
+  }, [path]);
+
+  const saveFile = () => {
+    const file = {
+      title: fileName,
+      category_type: category,
+      path: path,
+      project_id: 1 
+    }
+
+    axios.post('/api/documents', file)
+      .then(res => {
+        console.log(`post request success! here's the response ${res}`)
+
+        handleClose()
+
+        window.location.reload()
+      })
+      .catch(err => {
+        console.log(`Save file Error: ${err}`)
+      })
+    
+    
+  }
 
   return (
     <div>
@@ -71,7 +99,7 @@ export default function AddNewDocument(props) {
           <form className={classes.container}>
             <FormControl className={classes.formControl}>
               <InputLabel id="file-name">File Name</InputLabel>
-              <Input />
+              <Input value={fileName} onChange={event => setFileName(event.target.value)} />
             </FormControl>
 
             <FormControl className={classes.formControl}>
@@ -97,17 +125,15 @@ export default function AddNewDocument(props) {
             </FormControl>
           </form>
         </DialogContent>
-          <UploadButtons sendToParent={handlePath}/>
-         
-          
-        
+
+        <UploadButtons sendToParent={handlePath} />
 
         <DialogActions>
           <Button onClick={handleClose} color="primary">
             Cancel
           </Button>
-          <Button onClick={handleClose} color="primary">
-            Ok
+          <Button onClick={saveFile} color="primary">
+            Save
           </Button>
         </DialogActions>
       </Dialog>
--- client/src/components/Documents.js
+++ client/src/components/Documents.js
@@ -1,33 +1,35 @@
 import { Container } from "@material-ui/core";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { useEffect, useState } from "react";
 import "./App.scss";
 import AddNewDocument from "./Document/AddNewDocument";
 import DocumentList from "./Document/DocumentList";
 import SearchBar from "./Document/Searchbar";
-
+import axios from 'axios';
 const docData = [
   {
     id: 1,
-    title: "Big Contract",
-    category_type: "Contract",
+    title: "Random small Contract",
+    category_type: "Contract444",
     path: "user/dmar/documents",
     project_id: 1,
   },
   {
     id: 2,
-    title: "Floor Plan Option #2",
-    category_type: "Floor Plan",
+    title: "whatever Floor Plan Option #2",
+    category_type: "Floor Plan222",
     path: "user/dmar/documents/floorplans",
     project_id: 1,
   },
   {
     id: 3,
-    title: 'Window Options',
-    category_type: 'Supplementary',
+    title: 'BIG small Window Options',
+    category_type: 'Supplementary123',
     path: 'user/dmar/documents/windows',
     project_id: 2
   }
 ];
+
 
 const theme = createMuiTheme({
   overrides: {
@@ -43,7 +45,15 @@ const theme = createMuiTheme({
 
 export default function Documents() {
   // const classes = useStyles
-  
+  const [state, setState] = useState([])
+
+  useEffect(()=> {
+    axios.get("/api/documents")
+      .then(res => {
+        // console.log('request success! here is the response: ',res.data)
+        setState(res.data)
+      })
+  },[]);
   return (
     <section className="content">
       <h1>Documents</h1>
@@ -51,11 +61,11 @@ export default function Documents() {
       <ThemeProvider theme={theme}>
         <Container maxWidth="lg" disableGutters={true}>
           <SearchBar />
-          <AddNewDocument data={docData} />
+          <AddNewDocument data={state} />
         </Container>
       </ThemeProvider>
       
-      <DocumentList data={docData} />
+      <DocumentList data={state} />
       
     </section>
   );
