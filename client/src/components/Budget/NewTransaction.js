445ce1e62749b44d8ae0103bf92830113865c69b
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-06-08 21:39:34 -0700

    testing changes to db

--- client/src/components/Budget/NewTransaction.js
+++ client/src/components/Budget/NewTransaction.js
@@ -1,17 +1,26 @@
-import { useState } from 'react';
-import { makeStyles, Box, TextField, Button, InputLabel, FormControl, Select, MenuItem } from '@material-ui/core';
+import { useState } from "react";
+import {
+  makeStyles,
+  Box,
+  TextField,
+  Button,
+  InputLabel,
+  FormControl,
+  Select,
+  MenuItem,
+} from "@material-ui/core";
 
 const useStyle = makeStyles((theme) => ({
   container: {
-    display: 'flex',
-    flexDirection: 'column',
-    '& > *': {
-      marginTop: 25 
-    }
+    display: "flex",
+    flexDirection: "column",
+    "& > *": {
+      marginTop: 25,
+    },
   },
   button: {
-    background: '#427aa1',
-    color: '#fff'
+    background: "#427aa1",
+    color: "#fff",
   },
   formControl: {
     margin: theme.spacing(1),
@@ -22,33 +31,31 @@ const useStyle = makeStyles((theme) => ({
 export default function NewTransaction({ categories, addTransaction }) {
   const classes = useStyle();
 
-  const [description, setDescription] = useState('');
-  const [amount, setAmount] = useState('');
-  const [category, setCategory] = useState('');
-  const [date, setDate] = useState('');
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState("");
+  const [category, setCategory] = useState("");
+  const [date, setDate] = useState("");
 
   const resetForm = () => {
-    setDescription('');
-    setAmount('');
-    setCategory('');
-    setDate('');
+    setDescription("");
+    setAmount("");
+    setCategory("");
+    setDate("");
   };
 
   const newTransaction = () => {
     const transaction = {
-      description: description, 
+      description: description,
       amount: amount,
       date: date,
       budget_category_id: category,
-      milestone_id: 1,
-      change_order_id: 1
-    }
+    };
 
     addTransaction(transaction);
 
     resetForm();
-  }
-  
+  };
+
   return (
     <Box className={classes.container}>
       <form className={classes.container}>
@@ -63,10 +70,18 @@ export default function NewTransaction({ categories, addTransaction }) {
           />
         </FormControl>
         <FormControl className={classes.formControl}>
-          <TextField label="Description" value={description} onChange={(e) => setDescription(e.target.value)} />
+          <TextField
+            label="Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
         </FormControl>
         <FormControl className={classes.formControl}>
-          <TextField label="Amount" value={amount} onChange={(e) => setAmount(e.target.value)}/>
+          <TextField
+            label="Amount"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
         </FormControl>
         <FormControl className={classes.formControl}>
           <InputLabel id="budget_category">Category</InputLabel>
@@ -75,14 +90,22 @@ export default function NewTransaction({ categories, addTransaction }) {
             id="budget_category"
             value={category}
             onChange={(e) => setCategory(e.target.value)}
-            >
+          >
             {categories.map((category) => (
-              <MenuItem key={category.id} value={category.id}>{category.description}</MenuItem>
-              ))}
+              <MenuItem key={category.id} value={category.id}>
+                {category.description}
+              </MenuItem>
+            ))}
           </Select>
         </FormControl>
-        <Button className={classes.button} onClick={newTransaction} variant="contained">Add new transaction</Button>
+        <Button
+          className={classes.button}
+          onClick={newTransaction}
+          variant="contained"
+        >
+          Add new transaction
+        </Button>
       </form>
     </Box>
-  )
+  );
 }
