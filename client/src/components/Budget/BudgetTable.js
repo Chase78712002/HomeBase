f54f0bc2b892de5effe18281679c5992f4f74a1a
Lily Habberfield <lilyhabbs@gmail.com>
2021-06-11 16:53:45 +0000

    Display proper transactions per category

--- client/src/components/Budget/BudgetTable.js
+++ client/src/components/Budget/BudgetTable.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import { 
   makeStyles,
@@ -17,7 +17,7 @@ import IconButton from '@material-ui/core/IconButton';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 
-import TransactionRowItem from './TransactionRowItem';
+// import TransactionRowItem from './TransactionRowItem';
 
 const useStyle = makeStyles({
   table: {
@@ -31,10 +31,24 @@ const useStyle = makeStyles({
 
 export default function BudgetTable({categories, transactions}) {
   const classes = useStyle();
-
+  
   const [open, setOpen] = useState(false);
-  const [currentCategory, setCurrentCategory] = useState();
+  const [currentCategoryId, setCurrentCategoryId] = useState('');
+  const [currentCategoryTransactions, setCurrentCategoryTransactions] = useState(transactions);
 
+  const handleView = () => {
+    console.log('Id: ', currentCategoryId);
+    const filteredTransactions = transactions.filter(transaction => transaction.budget_category_id === currentCategoryId);
+    console.log(filteredTransactions);
+    setCurrentCategoryTransactions(filteredTransactions);
+}
+  
+  useEffect(() => {
+    console.log('CurrentId: ', currentCategoryId);
+    handleView();
+    
+  }, [currentCategoryId]);
+  
   if (categories.length === 0 || transactions.length === 0) {
     return (
       <div>
@@ -42,6 +56,19 @@ export default function BudgetTable({categories, transactions}) {
       </div>
     )
   }
+
+  // const handleOpen = (id) => {
+  //   setCurrentCategoryId(id);
+  //   setOpen(!open);
+  // }
+
+  // const handleView = () => {
+  //     console.log('Id: ', currentCategoryId);
+  //     const filteredTransactions = transactions.filter(transaction => transaction.budget_category_id === currentCategoryId);
+  //     console.log(filteredTransactions);
+  //     setCurrentCategoryTransactions(filteredTransactions);
+  //     setOpen(!open);
+  // }
   
   // calculating table totals
   const totalCosts = (value) => {
@@ -55,35 +82,45 @@ export default function BudgetTable({categories, transactions}) {
   return (
     <TableBody>
       {categories.map((category) => (
-        <TableRow key={category.id}>
-          <TableCell>
-            <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
-              {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-            </IconButton>
-          </TableCell>
-          <TableCell component="th" scope="row">
-            {category.description}
-          </TableCell>
-          <TableCell align="right">${category.estimate_amount}</TableCell>
-          <TableCell align="right">${category.actual_amount}</TableCell>
-          <TableCell align="right">${category.estimate_amount - category.actual_amount}</TableCell>
-        </TableRow>
+      <TableRow key={category.id}>
+        <TableCell>
+          <IconButton aria-label="expand row" size="small" onClick={() => { setCurrentCategoryId(category.id); setOpen(!open);}}>
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {category.description}
+        </TableCell>
+        <TableCell align="right">${category.estimate_amount}</TableCell>
+        <TableCell align="right">${category.actual_amount}</TableCell>
+        <TableCell align="right">${category.estimate_amount - category.actual_amount}</TableCell>
+      </TableRow>
       ))}
       <TableRow>
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
           <Collapse in={open} timeout="auto" unmountOnExit>
             <Box margin={1}>
-              <Typography variant="h6" gutterBottom component="div">Transaction history</Typography>
-              <Table size="small" aria-label="transactions">
-              <TableHead>
-                <TableRow>
-                  <TableCell colspan="2">Description</TableCell>
-                  <TableCell colspan="2">Date</TableCell>
-                  <TableCell align="right">Amount</TableCell>
-                </TableRow>
-              </TableHead>
-              <TransactionRowItem transactions={transactions}/>
-            </Table>
+              <Typography variant="h6" gutterBottom component="div">
+                Transaction history
+              </Typography>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell colSpan="2">Description</TableCell>
+                    <TableCell colSpan="2">Date</TableCell>
+                    <TableCell align="right">Amount</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {currentCategoryTransactions.map((transaction) => (
+                  <TableRow key={transaction.id}>
+                    <TableCell colSpan="2">{transaction.description}</TableCell>
+                    <TableCell colSpan="2">{transaction.date}</TableCell>
+                    <TableCell align="right">${transaction.amount}</TableCell>
+                  </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
             </Box>
           </Collapse>
         </TableCell>
@@ -91,10 +128,10 @@ export default function BudgetTable({categories, transactions}) {
       <TableRow>
         <TableCell></TableCell>
         <TableCell><strong>TOTALS:</strong></TableCell>
-        <TableCell align="right">${totalEstimate}</TableCell>
-        <TableCell align="right">${totalActual}</TableCell>
-        <TableCell align="right">${totalVariance}</TableCell>
-      </TableRow>
+        <TableCell align="right"><strong>${totalEstimate}</strong></TableCell>
+        <TableCell align="right"><strong>${totalActual}</strong></TableCell>
+        <TableCell align="right"><strong>${totalVariance}</strong></TableCell>
+    </TableRow>
     </TableBody>
-  )
+  );
 }
