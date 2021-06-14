1bd64e2a370235cec3f1f3de27adbeba0d5920a7
Lily Habberfield <lilyhabbs@gmail.com>
2021-06-14 08:41:22 -0600

    Update styling to be consistent with other pages

--- client/src/components/Document/Delete.jsx
+++ client/src/components/Document/Delete.jsx
@@ -1,23 +1,21 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import IconButton from "@material-ui/core/IconButton";
-import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
+import DeleteTwoToneIcon from '@material-ui/icons/DeleteTwoTone';
+
 const useStyles = makeStyles((theme) => ({
   root: {
     "& > *": {
       margin: theme.spacing(1),
     },
   },
-  icon: {
-    color:'red'
-  }
 }));
 
 export default function DeleteButton(props) {
   const classes = useStyles();
   return (
-    <IconButton onClick={() => props.delete(props.id)}className={classes.icon}>
-      <DeleteOutlineIcon />
+    <IconButton onClick={() => props.delete(props.id)}>
+      <DeleteTwoToneIcon color="error" />
     </IconButton>
   );
 }
